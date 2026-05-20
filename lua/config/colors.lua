4a41f7c69d7009f4151d285fe24d554210662f49
Filipe <kimile@gmail.com>
2026-05-20 10:20:26 +0100

    Remove background transparency

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -2,13 +2,8 @@ return {
     {
         "folke/tokyonight.nvim",
         config = function()
-            require("tokyonight").setup({
-                transparent = true,
-            })
+            require("tokyonight").setup({})
             vim.cmd.colorscheme "tokyonight"
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
     },
 }
