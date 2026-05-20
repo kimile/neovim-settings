7cf14206410a0655c656bdc311c0284aa86db615
Filipe <kimile@gmail.com>
2026-05-20 13:36:55 +0100

    prevent harpoon from hopening in tree panel

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -20,6 +20,11 @@ return {
                 enable = true,
                 update_root = false,
             },
+            on_attach = function(bufnr)
+                local api = require("nvim-tree.api")
+                api.config.mappings.default_on_attach(bufnr)
+                vim.keymap.del("n", "<C-e>", { buffer = bufnr })
+            end,
         })
 
         -- Keymaps
